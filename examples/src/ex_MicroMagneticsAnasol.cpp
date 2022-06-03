f2f02c0e41195de8c163b86b8455f1bc7f352221
AlexanderMueller <rathet@gmail.com>
2022-06-03 14:22:09 +0200

    improvements in energy calculation of nanodot

--- examples/src/ex_MicroMagneticsAnasol.cpp
+++ examples/src/ex_MicroMagneticsAnasol.cpp
@@ -21,10 +21,13 @@
 #include <autodiff/common/eigen.hpp>
 #include <dune/common/power.hh>
 
-template<typename ScalarType,typename F, typename DF>
-auto exchangeEnergy(F&& f, DF&& df, const ScalarType rho)
+template<typename F, typename DF>
+auto exchangeEnergy(F&& f, DF&& df,  double rho) -> decltype(f(rho))
 {
-  return (Dune::power(df(rho),2)*rho*rho - Dune::power(cos(f(rho)),2) + 1.0)/(2.0*rho);
+  if (rho==0.0)
+    return 0.0;
+    else
+  return (df(rho)*df(rho)*rho*rho - cos(f(rho))*cos(f(rho)) + 1.0)/(2.0*rho);
 }
 
 
@@ -49,7 +52,7 @@ auto fourierAnsatzDerivative(const Eigen::VectorX<ScalarType>& d,double rho,doub
   const double pi = std::numbers::pi;
 
   for (int i = 0; i < d.size(); ++i)
-    res+=((2*i + 1)*pi*cos(rho*(2*i + 1)*pi/(2*R))*d[i])/(2.0*R);
+    res+=((2.*i + 1.)*pi*cos(rho*(2.*i + 1.)*pi/(2.0*R))*d[i])/(2.0*R);
 
   return res;
 
@@ -60,60 +63,81 @@ template<typename F, typename DF>
 auto energyIntegrator(F&& f, DF&& df, const double R, const double tol)
 {
   auto exE= [&] (auto rho) { return exchangeEnergy( f,  df, rho);};
-  return AdaptiveIntegrator::integrate(exE, 0, R, tol);
+  AdaptiveIntegrator::IntegratorC integrator;
+  return integrator.integrate(exE, 0, R, tol);
 }
 
 
 int main(int argc, char **argv) {
 
-  const double R = 1;
+  const double R = 2;
   Eigen::VectorXd xd(2);
-  xd << 1,0;
+  xd << 1,1;
 
   auto f = [&](auto x ){return fourierAnsatz<double>(xd,x,R);};
   auto df = [&](auto x ){return fourierAnsatzDerivative<double>(xd,x,R);};
 
-  Ikarus::plot::drawFunction(f, {0,R}, 100);
-  Ikarus::plot::drawFunction(df, {0,R}, 100);
-
-
-  std::cout << "First we approximate the integral of f(x) = x^2 on [0,2]" << std::endl;
-
-  std::cout<<"IntVal: "<<AdaptiveIntegrator::integrate(f, 0, R, 1e-8)<<std::endl;
-  std::cout<<"IntVal: "<<AdaptiveIntegrator::integrate(f, 0, R, 1e-8)<<std::endl;
-
-
-
-
+//  Ikarus::plot::drawFunction(f, {0,R}, 100);
+//  Ikarus::plot::drawFunction(df, {0,R}, 100);
 
 
   auto energy = [&](auto& d){
-    auto fdual = [&](auto x ){return fourierAnsatz(d,x,R);};
+    auto fdual = [&](auto x ) {return fourierAnsatz(d,x,R);};
     auto dfdual = [&](auto x ){return fourierAnsatzDerivative(d,x,R);};
     return energyIntegrator(fdual,dfdual,R,1e-8);};
+
+  auto grad = [&](auto&& d) {
+    auto xdR = d.template cast<autodiff::dual>().eval();
+    return autodiff::gradient(energy, wrt(xdR), at(xdR));
+  };
+
+  auto hess = [&](auto&& d) {
+    auto xdR = d.template cast<autodiff::dual2nd>().eval();
+    Eigen::SparseMatrix<double> hSparse=autodiff::hessian(energy, wrt(xdR), at(xdR)).sparseView();
+    return hSparse;
+  };
+
+  auto nonLinOp = Ikarus::NonLinearOperator(linearAlgebraFunctions(energy, grad,hess), parameter(xd));
+
+  checkGradient(nonLinOp, {.draw = false, .writeSlopeStatement = true});
+  checkHessian(nonLinOp, {.draw = false, .writeSlopeStatement = true});
+
+  auto tr = Ikarus::makeTrustRegion(nonLinOp);
+  tr->setup({.verbosity = 1, .Delta0 = 1});
+  const auto solverInfo = tr->solve();
+
+  std::cout<<"Resulting coeffs: \n"<<xd<<std::endl;
+
+//  std::cout << "First we approximate the integral of f(x) = x^2 on [0,2]" << std::endl;
+//  AdaptiveIntegrator::IntegratorC integrator;
+  std::cout<<"ExchangeEnergy: "<<energy(xd)<<std::endl;
+//  integrator.reset();
+//  std::cout<<"IntVal: "<<integrator.integrate(f, 0, R, 1e-8)<<std::endl;
+//  integrator.reset();
 
   Eigen::VectorXdual xdual(xd.size());
   xdual =xd;
   Eigen::VectorXd g = gradient(energy, autodiff::wrt(xdual), autodiff::at(xdual));
 
-  const int tols = 5;
-  std::vector<double> result1(tols);
-  std::vector<double> error1(tols);
-  std::vector<double> tolerance(tols);
-  for (int i = 0; i<tols; i++)
-  {
-    tolerance[i]=pow(10,-i);
-//    result1 [i] = helper(0, R, f,tolerance[i] );
-    std::cout<<"IntVal: "<<AdaptiveIntegrator::integrate(f, 0, R, tolerance[i])<<std::endl;
-    std::cout << "(" << result1 [i] << "," << tolerance[i] << ")" << std::endl;
-//    error1 [i] = fabs(true_value1 - result1 [i]);
-  }
-
-  std::cout << "(Approximate integral of x^2, tolerance, error )" << std::endl;
-  for (int i = 0; i<tols; i++)
-  {
-
-  }
+//  const int tols = 5;
+//  std::vector<double> result1(tols);
+//  std::vector<double> error1(tols);
+//  std::vector<double> tolerance(tols);
+////  for (int i = 0; i<tols; i++)
+////  {
+////    tolerance[i]=pow(10,-i);
+//////    result1 [i] = helper(0, R, f,tolerance[i] );
+////    std::cout<<"IntVal: "<<integrator.integrate(f, 0, R, tolerance[i])<<std::endl;
+////    integrator.reset();
+////    std::cout << "(" << result1 [i] << "," << tolerance[i] << ")" << std::endl;
+//////    error1 [i] = fabs(true_value1 - result1 [i]);
+////  }
+//
+//  std::cout << "(Approximate integral of x^2, tolerance, error )" << std::endl;
+//  for (int i = 0; i<tols; i++)
+//  {
+//
+//  }
 
 
 }
--- src/include/ikarus/utils/integrators/AdaptiveIntegrator.hpp
+++ src/include/ikarus/utils/integrators/AdaptiveIntegrator.hpp
@@ -40,35 +40,41 @@ namespace AdaptiveIntegrator
   const double x2 = .64185334234578130578123554132903188354;
   const double x3 = .23638319966214988028222377349205292599;
 
+  class IntegratorC{
 
-template <class T>
-  auto adaptlobstp(T &&f, const double a, const double b,
-                   const double fa, const double fb, const double is )
+    bool terminated{false};
+
+  public:
+    void reset(){
+      terminated= false;
+    }
+template <class T, typename ScalarType>
+  ScalarType adaptlobstp(T &&f, const double a, const double b,
+                   ScalarType fa, ScalarType fb, ScalarType is )
 {
   double m, h;
   m = (a+b)/2.; h = (b-a)/2.;
 
-  auto mll = m - alpha*h;
-  auto ml = m - beta*h;
-  auto mr = m + beta*h;
-  auto mrr = m + alpha*h;
+  double mll = m - alpha*h;
+  double ml = m - beta*h;
+  double mr = m + beta*h;
+  double mrr = m + alpha*h;
 
-  auto fmll = f(mll);
-  auto fml = f(ml);
-  auto fm = f(m);
-  auto fmr = f(mr);
-  auto fmrr = f(mrr);
+  ScalarType fmll = f(mll);
+  ScalarType fml = f(ml);
+  ScalarType fm = f(m);
+  ScalarType fmr = f(mr);
+  ScalarType fmrr = f(mrr);
 
-  auto i2 = (h/6.)*(fa + fb + 5.*(fml+fmr));
-  auto i1 = (h/1470.)*(77*(fa+fb) + 432.*(fmll + fmrr) + 625.*(fml + fmr) + 672.*fm);
+  ScalarType i2 = (h/6.)*(fa + fb + 5.*(fml+fmr));
+  ScalarType i1 = (h/1470.)*(77.*(fa+fb) + 432.*(fmll + fmrr) + 625.*(fml + fmr) + 672.*fm);
 
   if (is + (i1-i2) == is or mll <= a or b <= mrr)
   {
-//    if ( (m <= a or b <= m) and !terminated)
-    if ( (m <= a or b <= m))
+    if ( (m <= a or b <= m) and !terminated)
     {
       cerr << "No machine number in the interval. Requested tolerance may not be met.\n";
-//      terminated = true;
+      terminated = true;
     }
     return i1;
   }
@@ -90,7 +96,7 @@ template <class T>
 auto integrate(T &&f, const double a, const double b, const double tol_)
 {
 
-
+  using ScalarType = decltype(f(a));
 
   double tol, eps;
     eps = numeric_limits<double>::epsilon();
@@ -98,33 +104,45 @@ auto integrate(T &&f, const double a, const double b, const double tol_)
 
     double m, h;
     m = (a+b)/2.; h = (b-a)/2.;
-    std::array<decltype(f(a)),13> y = {f(a),f(m-x1*h),f(m-alpha*h),f(m-x2*h),f(m-beta*h),f(m-x3*h),f(m),
+    std::array<ScalarType,13> y = {f(a),f(m-x1*h),f(m-alpha*h),f(m-x2*h),f(m-beta*h),f(m-x3*h),f(m),
                     f(m+x3*h),f(m+beta*h),f(m+x2*h),f(m+alpha*h),f(m+x1*h) ,f(b)};
 
-    auto fa = y[0];
-    auto fb = y[12];
+    ScalarType fa = y[0];
+    ScalarType fb = y[12];
 
-
-    auto i2 = (h/6.)*(y[0] + y[12] + 5.*(y[4] + y[8]));
-    auto i1 = (h/1470.)*(77.*(y[0]+y[12]) + 432.*(y[2]+y[10]) + 625.*(y[4]+y[8]) + 672.*y[6]);
-    auto is = h*(.0158271919734802*(y[0]+y[12]) + .0942738402188500*(y[1]+y[11])
+    ScalarType i2 = (h/6.)*(y[0] + y[12] + 5.*(y[4] + y[8]));
+    ScalarType i1 = (h/1470.)*(77.*(y[0]+y[12]) + 432.*(y[2]+y[10]) + 625.*(y[4]+y[8]) + 672.*y[6]);
+    ScalarType is = h*(.0158271919734802*(y[0]+y[12]) + .0942738402188500*(y[1]+y[11])
            + .155071987336585*(y[2]+y[10]) + .188821573960182*(y[3]+y[9])
            + .199773405226859*(y[4]+y[8]) + .224926465333340*(y[5]+y[7])
            + .242611071901408*y[6]);    
 
-    auto erri1 = abs(i1 - is);
-    auto erri2 = abs(i2 - is);
-    auto R = (erri2 != 0.) ? erri1/erri2 : 1.;
-    
+
+    double R;
+    if constexpr (std::is_same_v<ScalarType,double>) {
+      double erri1 = abs(i1 - is);
+      double erri2 = abs(i2 - is);
+      R = (erri2 != 0.) ? erri1 / erri2 : 1.;
+    } else if constexpr (std::is_same_v<ScalarType,autodiff::dual>)
+    {      double erri1 = abs(i1.val - is.val);
+      double erri2 = abs(i2.val - is.val);
+      R = (erri2 != 0.) ? erri1 / erri2 : 1.;
+    }else if constexpr (std::is_same_v<ScalarType,autodiff::dual2nd>)
+    {      double erri1 = abs(i1.val.val - is.val.val);
+      double erri2 = abs(i2.val.val - is.val.val);
+      R = (erri2 != 0.) ? erri1 / erri2 : 1.;
+    }
+
+
     tol = (R > 0. and R < 1.) ? tol_/R : tol_; 
-    is = fabs(is)*tol/eps;
+    is = abs(is)*tol/eps;
     if (is == 0.) is = b-a;
 
     return adaptlobstp(f, a, b, fa, fb, is);
 
 }
 
-
+  };
 }
 
 
