f872bac92213ba62e1f38e20813b6a0159b33b12
Tarun Mitruka <mitruka@ibb.uni-stuttgart.de>
2022-07-01 14:34:57 +0200

    added center position deflection w calculation

--- examples/src/simply_supported_Plate.cpp
+++ examples/src/simply_supported_Plate.cpp
@@ -1,19 +1,30 @@
+// /*
+//  *  This file is part of the Ikarus distribution (https://github.com/rath3t/Ikarus).
+//  *  Copyright (c) 2021 Alexander Müller.
+//  *  Institut fuer Baustatik und Baudynamik
+//  *  Universität Stuttgart
+//  *
+//  *  This library is free software; you can redistribute it and/or
+//  *   modify it under the terms of the GNU Lesser General Public
+//  *   License as published by the Free Software Foundation; either
+//  *   version 2.1 of the License, or (at your option) any later version.
 //
-// Created by ac136645 on 7/1/2022.
+// *   This library is distributed in the hope that it will be useful,
+// *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// *   Lesser General Public License for more details.
 //
+// *   You should have received a copy of the GNU Lesser General Public
+// *   License along with this library; if not, write to the Free Software
+// *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+// *  USA
+// *
 
 #include <config.h>
 
-#include "src/include/ikarus/finiteElements/feTraits.hh"
-
-#include <matplot/matplot.h>
-#include <numbers>
-
-#include <dune/common/indices.hh>
 #include <dune/functions/functionspacebases/boundarydofs.hh>
 #include <dune/functions/functionspacebases/subspacebasis.hh>
-#include <dune/functions/gridfunctions/analyticgridviewfunction.hh>
-#include <dune/geometry/quadraturerules.hh>
+#include <dune/functions/functionspacebases/interpolate.hh>
 #include <dune/iga/igaalgorithms.hh>
 #include <dune/iga/nurbsgrid.hh>
 
@@ -21,19 +32,11 @@
 #include <Eigen/Dense>
 
 #include <ikarus/assembler/simpleAssemblers.hh>
-#include <ikarus/controlRoutines/loadControl.hh>
-#include <ikarus/finiteElements/feBases/autodiffFE.hh>
-#include <ikarus/finiteElements/feBases/scalarFE.hh>
 #include <ikarus/linearAlgebra/nonLinearOperator.hh>
 #include <ikarus/localBasis/localBasis.hh>
-#include <ikarus/solver/nonLinearSolver/newtonRaphson.hh>
-#include <ikarus/utils/algorithms.hh>
-#include <ikarus/utils/concepts.hh>
 #include <ikarus/utils/drawing/griddrawer.hh>
 #include <ikarus/utils/eigenDuneTransformations.hh>
 #include <ikarus/utils/observer/controlVTKWriter.hh>
-#include <ikarus/utils/observer/loadControlObserver.hh>
-#include <ikarus/utils/observer/nonLinearSolverLogger.hh>
 #include <ikarus/finiteElements/mechanics/kirchhoffPlate.hh>
 
 // Reference: Echter, R., Oesterle, B., Bischoff, M., 2013. A hierarchic family of isogeometric shell finite elements.
@@ -69,7 +72,7 @@ int main() {
   patchData = Dune::IGA::degreeElevate(patchData, 1, 1);
   Grid grid(patchData);
 
-  grid.globalRefine(2);
+  grid.globalRefine(3);
 
   auto gridView = grid.leafGridView();
   //    draw(gridView);
@@ -80,6 +83,7 @@ int main() {
   std::vector<bool> dirichletFlags(basis.size(), false);
   Dune::Functions::forEachBoundaryDOF(basis, [&](auto&& index) { dirichletFlags[index] = true; });
 
+  std::cout<<"#################################################";
   std::cout << "This gridview contains: " << std::endl;
   std::cout << gridView.size(2) << " vertices" << std::endl;
   std::cout << gridView.size(1) << " edges" << std::endl;
@@ -90,7 +94,7 @@ int main() {
   auto localView         = basis.localView();
   const double Emod      = 1000;
   const double nu        = 0.3;
-  const double thickness = 0.001;
+  const double thickness = 0.1;
   std::vector<KirchhoffPlate<decltype(basis)>> fes;
   for (auto& ele : elements(gridView))
     fes.emplace_back(basis, ele, Emod, nu, thickness);
@@ -139,19 +143,26 @@ int main() {
   const double factor_ana = (qz * Dune::power(L,4) * 12.0 * (1.0 - nu*nu))/(Emod * Dune::power(thickness,3));
   const double w_max_ana = ((5.0/384.0) - (4.0/Dune::power(pi,5)) * (0.68562 + 0.00025)) * factor_ana;
 
+  /// Find displacement w at the center of the plate (x=y=5.0=Lmid)
   auto wGlobalFunction = Dune::Functions::makeDiscreteGlobalBasisFunction<Dune::FieldVector<double, 1>>(basis, w);
   auto localw          = localFunction(wGlobalFunction);
-
   double w_fe = 0.0;
+  const double Lmid = 5.0;
+  Eigen::Vector2d c_pos;
+  c_pos[0] = c_pos[1] = Lmid;
 
   for (auto& ele : elements(gridView)) {
     localView.bind(ele);
     localw.bind(ele);
     const auto geo   = localView.element().geometry();
-//    std::cout<<"Center:  "<<geo.center()<<std::endl;
-//    w_fe = localw(geo.center());
+    if (((geo.corner(0)[0] <= Lmid) and (Lmid <= geo.corner(1)[0])) and ((geo.corner(0)[1] <= Lmid) and (Lmid <= geo.corner(2)[1])))
+    {
+        const auto local_pos = geo.local(toFieldVector(c_pos));
+        w_fe = localw(local_pos);
+    }
   }
-
-  std::cout << "w_max_ana: " << w_max_ana << std::endl<<" w_fe: " << w_fe << std::endl;
-
+  std::cout<<"#################################################";
+  std::cout<<std::endl<<"w_max_ana: " << w_max_ana << std::endl<<"w_fe     : " << w_fe << std::endl;
+  std::cout<<"The error is:"<<sqrt(Dune::power((w_max_ana-w_fe),2))<<std::endl;
+  std::cout<<"#################################################";
 }
